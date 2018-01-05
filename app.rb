10bd4ed53f5535902bbee1d34e94f6cd4e77706e
Hoon Lee <yl.hoony@gmail.com>
2018-01-05 06:15:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -36,8 +36,8 @@ class App < Sinatra::Base
     binding.pry
     # %2A
     @operation = params[:operation]
-    @num1 = params[:num1].to_i
-    @num2 = params[:num2].to_i
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
     erb :operation
   end
 
